bcaee8bcce3d72c9e6bbabda6ebfb5462f9aa5c1
Alf Equilfe <96450945+equneko@users.noreply.github.com>
2022-12-11 19:39:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-alfa.use('#std','dom','dom.render');
+alfa.use('#std','dom','dom.render','math.random');
+
+log("Hello World");
+var range = put("Type Number Randomly");
+log("Your Random Number: "+random()*range);
 
 dom.init('h1','p');
-
 render(
-    h1("Halo Dunia"),
-    p("Belajar <b>AlfaScript</b>!")
+  h1("Hello World!"),
+  p("Learn Weird Language <b>AlfaScript!</b>")
 );
